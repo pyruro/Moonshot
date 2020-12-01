802c8a50d577aa90cd15c24b4d05abad7bcdcb17
r1un0wn <r1un0wn@example.com>
2020-12-01 19:28:14 +0100

    backup

--- piezas_puzle.gd
+++ piezas_puzle.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
 onready var pieza1_correcta = 0
+onready var pieza2_correcta = 0
+onready var pieza3_correcta = 0
+onready var pieza4_correcta = 0
+onready var pieza5_correcta = 0
+onready var pieza6_correcta = 0
+onready var pieza7_correcta = 0
+onready var pieza8_correcta = 0
+onready var pieza9_correcta = 0
 
-signal signal_pieza_correcta(pieza_correcta)
+#signal signal_pieza_correcta(pieza_correcta)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -21,5 +29,52 @@ func _ready():
 func _on_lugar_p1_body_entered(body):
 	if body.rotation_degrees == 0 and body.name == "colocarP1":
 		pieza1_correcta = 1
-		emit_signal("signal_pieza_correcta", pieza1_correcta)
+	pass # Replace with function body.
+
+
+func _on_lugar_p2_body_entered(body):
+	if body.rotation_degrees == 0 and body.name == "colocarP2":
+		pieza2_correcta = 1
+	pass # Replace with function body.
+
+
+func _on_lugar_p3_body_entered(body):
+	if body.rotation_degrees == 0 and body.name == "colocarP3":
+		pieza3_correcta = 1
+	pass # Replace with function body.
+
+
+func _on_lugar_p4_body_entered(body):
+	if body.rotation_degrees == 0 and body.name == "colocarP4":
+		pieza4_correcta = 1
+	pass # Replace with function body.
+
+
+func _on_lugar_p5_body_entered(body):
+	if body.rotation_degrees == 0 and body.name == "colocarP5":
+		pieza5_correcta = 1
+	pass # Replace with function body.
+
+
+func _on_lugar_p6_body_entered(body):
+	if body.rotation_degrees == 0 and body.name == "colocarP6":
+		pieza6_correcta = 1
+	pass # Replace with function body.
+
+
+func _on_lugar_p7_body_entered(body):
+	if body.rotation_degrees == 0 and body.name == "colocarP7":
+		pieza7_correcta = 1
+	pass # Replace with function body.
+
+
+func _on_lugar_p8_body_entered(body):
+	if body.rotation_degrees == 0 and body.name == "colocarP8":
+		pieza8_correcta = 1
+	pass # Replace with function body.
+
+
+func _on_lugar_p9_body_entered(body):
+	if body.rotation_degrees == 0 and body.name == "colocarP9":
+		pieza9_correcta = 1
 	pass # Replace with function body.
--- puzzle_minigame/main.gd
+++ puzzle_minigame/main.gd
@@ -27,25 +27,75 @@ onready var lugares_puzle = get_node("lugares_puzle")
 onready var nonulo
 
 onready var p1lp1 = lugares_puzle.pieza1_correcta
+onready var p2lp2 = lugares_puzle.pieza2_correcta
+onready var p3lp3 = lugares_puzle.pieza3_correcta
+onready var p4lp4 = lugares_puzle.pieza4_correcta
+onready var p5lp5 = lugares_puzle.pieza5_correcta
+onready var p6lp6 = lugares_puzle.pieza6_correcta
+onready var p7lp7 = lugares_puzle.pieza7_correcta
+onready var p8lp8 = lugares_puzle.pieza8_correcta
+onready var p9lp9 = lugares_puzle.pieza9_correcta
+
+var todo_correcto = []
 
 func _ready():
-	print(p1lp1)
-	print(p1.dragging)
 #	lugares_puzle.connect("signal_pieza_correcta",Node2D,[])
 	pass # Replace with function body.
 
 
-func _handle():
-	print("uweueuueueu")
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	print(p1lp1)
-	if p1lp1:
-		print("YESSSS")
+	p1lp1 = lugares_puzle.pieza1_correcta
+	p2lp2 = lugares_puzle.pieza2_correcta
+	p3lp3 = lugares_puzle.pieza3_correcta
+	p4lp4 = lugares_puzle.pieza4_correcta
+	p5lp5 = lugares_puzle.pieza5_correcta
+	p6lp6 = lugares_puzle.pieza6_correcta
+	p7lp7 = lugares_puzle.pieza7_correcta
+	p8lp8 = lugares_puzle.pieza8_correcta
+	p9lp9 = lugares_puzle.pieza9_correcta
+	
+	todo_correcto = [p1lp1,p2lp2,p3lp3,p4lp4,p5lp5,p6lp6,p7lp7,p8lp8,p9lp9]
+	
+	if p1lp1 and not p1.dragging:
+		p1.get_node("Sprite1").set_centered(false)
+		p1.position = lp1.global_position
+		p1.dragging=false
+	if p2lp2 and not p2.dragging:
+		p2.get_node("Sprite2").set_centered(false)
+		p2.position = lp2.global_position
+		p2.dragging=false
+	if p3lp3 and not p3.dragging:
+		p3.get_node("Sprite3").set_centered(false)
+		p3.position = lp3.global_position
+		p3.dragging=false
+	if p4lp4 and not p4.dragging:
+		p4.get_node("Sprite4").set_centered(false)
+		p4.position = lp4.global_position
+		p4.dragging=false
+	if p5lp5 and not p5.dragging:
+		p5.get_node("Sprite5").set_centered(false)
+		p5.position = lp5.global_position
+		p5.dragging=false
+	if p6lp6 and not p6.dragging:
+		p6.get_node("Sprite6").set_centered(false)
+		p6.position = lp6.global_position
+		p6.dragging=false
+	if p7lp7 and not p7.dragging:
+		p7.get_node("Sprite7").set_centered(false)
+		p7.position = lp7.global_position
+		p1.dragging=false
+	if p8lp8 and not p8.dragging:
+		p8.get_node("Sprite8").set_centered(false)
+		p8.position = lp8.global_position
+		p8.dragging=false
+	if p9lp9 and not p9.dragging:
+		p9.get_node("Sprite9").set_centered(false)
+		p9.position = lp9.global_position
+		p9.dragging=false
+	if p1lp1==1 and p2lp2==1 and p3lp3==1 and p4lp4==1 and p5lp5==1 and p6lp6==1 and p7lp7==1 and p8lp8==1 and p9lp9:
+		print("VICTORIA")
 		p1lp1 = 0
-	pass
 
 
 
